7956084e13de0dd5e78b8c0cb217a4e9b293370a
Cristian Magana <1132537@iheartmedia.com>
2022-02-07 13:34:15 -0800

    drains pod from node

--- k8s-scripts/refresh-eks-nodes.py
+++ k8s-scripts/refresh-eks-nodes.py
@@ -3,6 +3,9 @@ import sys
 import time
 import subprocess
 
+LOG="log.txt"
+PODLOGS="podlogs.txt"
+
 class bcolors:
     GREEN = '\033[92m'  # GREEN
     YELLOW = '\033[93m'  # YELLOW
@@ -11,21 +14,26 @@ class bcolors:
     LIGHT_BLUE = '\033[36m'
     ERROR = '\033[91m'
 
-LOG="log.txt"
-
 def refresh_loop(eksNode):
-    K8SPODCMD="""kubectl get po -A -o wide | grep {} | aws '{{print $1, $2}}'""".format(eksNode)
-    
+
     logFile = open(LOG, 'w')
+    K8SPODCMD="""kubectl get po -A -o wide --show-labels | grep {} | awk '{{print $2, $11}}'""".format(eksNode)
+    
     subprocess.call(K8SPODCMD, shell=True, stdout=logFile, stderr=logFile)
 
-    with open(LOG, 'w') as ns_inputs:
-        for ns in ns_inputs:           
-            ns = ns.strip()
-            print("-------------  Evaluating " + ns +" -------------  ")
-            get_tenant_info(tenantEnv, ns)
+    with open(LOG, 'r') as podsToDrain:
+        for line in podsToDrain:           
+            pod = line.split(" ")[0]
+            label = line.split(" ")[1].split(",")[0]
+            print(label)
+            drain_pod(eksNode, pod, label)
 
-
+def drain_pod(eksNode, pod, label):
+    podLog = open(PODLOGS, 'a')
+    ECHOCMD="""echo draining {} on node {}""".format(pod,eksNode)
+    subprocess.call(ECHOCMD, shell=True, stdout=podLog, stderr=podLog)
+    K8SDRAINCMD = """kubectl drain {} --ignore-daemonsets --delete-emptydir-data --pod-selector={}""".format(eksNode,label)
+    subprocess.call(K8SDRAINCMD, shell=True, stdout=podLog, stderr=podLog)
 
 if __name__ == '__main__':
-    refresh_loop(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    refresh_loop(sys.argv[1])
